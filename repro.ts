efe0f12b31b332cbd35f273963bc6b7244740e19
avallete <andrew.valleteau@supabase.io>
2025-09-15 11:49:09 +0200

    chore: update add ssr

--- repro.ts
+++ repro.ts
@@ -2,6 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./database.types";
 import { expectType } from "tsd";
 import { TypeEqual } from "ts-expect";
+import { createBrowserClient, createServerClient } from "@supabase/ssr";
 
 const client = createClient<Database>("", "");
 
@@ -19,3 +20,17 @@ let expected:
   | null;
 // ensure both types are equals
 expectType<TypeEqual<typeof data, typeof expected>>(true);
+
+const serverClient = createServerClient<Database>("", "", {
+  cookies: {
+    getAll: () => [],
+    setAll: () => {},
+  },
+});
+
+const { data: serverData } = await serverClient.from("users").select("*");
+expectType<TypeEqual<typeof serverData, typeof expected>>(true);
+
+const browserClient = createBrowserClient<Database>("", "");
+const { data: browserData } = await browserClient.from("users").select("*");
+expectType<TypeEqual<typeof browserData, typeof expected>>(true);
