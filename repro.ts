584651f928bacb93c5867030cd771e0ad164450b
avallete <andrew.valleteau@supabase.io>
2025-09-23 19:17:09 +0200

    chore: add gwax repro

--- repro.ts
+++ repro.ts
@@ -2,35 +2,74 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./database.types";
 import { expectType } from "tsd";
 import { TypeEqual } from "ts-expect";
-import { createBrowserClient, createServerClient } from "@supabase/ssr";
 
 const client = createClient<Database>("", "");
 
-// Expect data to be of type:
-// const data: {
-//     id: number;
-//     name: string | null;
-// }[] | null
-const { data } = await client.from("users").select("*");
-let expected:
-  | {
-      id: number;
-      name: string | null;
-    }[]
-  | null;
-// ensure both types are equals
+const { data } = await client
+  .from("countries")
+  .select(
+    `name,
+  country_alphas!inner (
+    alpha2,
+    alpha3
+  ),
+  country_alpha_locations(location)
+  `
+  )
+  .eq("name", "Canada")
+  .single();
+
+let expected: {
+  name: string;
+  country_alphas: {
+    alpha2: string;
+    alpha3: string;
+  };
+  country_alpha_locations: {
+    location: string;
+  } | null;
+} | null;
 expectType<TypeEqual<typeof data, typeof expected>>(true);
 
-const serverClient = createServerClient<Database>("", "", {
-  cookies: {
-    getAll: () => [],
-    setAll: () => {},
-  },
-});
+// RPC Call filtering
+const { data: data2 } = await client
+  .rpc("all_countries")
+  .select(
+    `
+  name,
+  country_alphas!inner (
+    alpha2,
+    alpha3
+  )
+  `
+  )
+  /**
+   * This line results in a type error because supabase fails to recognize read filters
+   * on rpc calls: https://postgrest.org/en/v12/references/api/functions.html
+   * Property 'eq' does not exist on type 'PostgrestTransformBuilder<{ Tables: { countries: { Row: { id: number; name: string; lowername: string | null; }; Insert: { id?: undefined; name: string; }; Update: { id?: undefined; name?: string | undefined; }; Relationships: []; }; country_alphas: { ...; }; }; Views: {}; Functions: { ...; }; Enums: {}; CompositeTy...'.ts(2339)
+   *
+   * Note: it works as expected despite the type error
+   */
+  .eq("name", "Canada")
+  .single();
 
-const { data: serverData } = await serverClient.from("users").select("*");
-expectType<TypeEqual<typeof serverData, typeof expected>>(true);
-
-const browserClient = createBrowserClient<Database>("", "");
-const { data: browserData } = await browserClient.from("users").select("*");
-expectType<TypeEqual<typeof browserData, typeof expected>>(true);
+const { data: data3 } = await client
+  .rpc("country_by_name", { name: "Canada" })
+  .select(
+    `
+    name,
+    lowername,
+    country_alphas!inner (
+      alpha2,
+      alpha3
+    )
+    `
+  )
+  .single();
+/**
+ * This line results in a type error because supabase fails to recognize the
+ * computed field on the rpc select: https://postgrest.org/en/v12/references/api/computed_fields.html
+ *
+ * Returning a type of:
+ * const caResponse: SelectQueryError<"column 'lowername' does not exist on 'country_by_name'.">
+ */
